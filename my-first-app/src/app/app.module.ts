6d59af7c7af56aeabe565aab2243e19394a054d3
Ramesses ii <kjayanth807@gmail.com>
2023-12-25 15:54:42 +0530

    assignment 1 completed

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { WarningComponent } from './alerts/warning/warning.component';
+import { SuccessComponent } from './alerts/success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
